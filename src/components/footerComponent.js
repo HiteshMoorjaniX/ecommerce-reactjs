94b09ea14a7e2569acbb99718dcb25a0abd17e7a
Hitesh Moorjani <moorjanihitesh21@gmail.com>
2019-10-05 12:37:24 +0530

    adding header and footer components

---
+++ src/components/footerComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+
+            <h3>This is Footer !</h3>
+
+        );
+    }
+}
+
+export default Footer;
